dc50441cf7bc0c917480fc07e706d3c4a75e06a6
KyungEun <comy392791@gmail.com>
2024-06-13 12:25:37 +0900

    #11- 레퍼지토리 인터페이스 생성
    DB에 접근하기 위해 빈 레퍼지토리를 생성만 함.

---
+++ src/main/java/com/bitstudy/board/repository/Ex01_4_ArticleRepository.java
@@ -0,0 +1,15 @@
+package com.bitstudy.board.repository;
+
+import com.bitstudy.board.domain.Ex01_1_Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//TDD할 때 사용할 임시 파일.(이용해서 dB 접근)
+public interface Ex01_4_ArticleRepository extends JpaRepository<Ex01_1_Article, Long> {
+
+}
+
+/*
+* TDD 하기
+*   - Ctrl+Shift+T 하기. ( 우클릭 go to)
+*   - Junit5 버전 확인 후 생성.
+* */
---
+++ src/main/java/com/bitstudy/board/repository/Ex01_5_ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.bitstudy.board.repository;
+
+import com.bitstudy.board.domain.Ex01_2_ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Ex01_5_ArticleCommentRepository extends JpaRepository<Ex01_2_ArticleComment, Long> {
+}
