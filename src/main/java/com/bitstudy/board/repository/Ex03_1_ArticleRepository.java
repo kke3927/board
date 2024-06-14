b9eddbcfde60c258714a5fc9bcf0f9079818c282
KyungEun <comy392791@gmail.com>
2024-06-14 15:41:49 +0900

    #2-data rest 를 위한 repository(비어있음) 만듦
    .

---
+++ src/main/java/com/bitstudy/board/repository/Ex03_1_ArticleRepository.java
@@ -0,0 +1,15 @@
+package com.bitstudy.board.repository;
+
+import com.bitstudy.board.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+/** @ RepositoryRestResource 를 넣어서 spring rest data를 준비하고 03_2 에서도 똑같이.
+ *
+ * service 탭에 있는 BoardApplication 실행하고,
+ * 브라우저에 localhost:8080/api 치면 HAL Explorer 켜질 것.
+ * */
+
+@RepositoryRestResource /* yaml 파일에서 detection-strategy : annotated 대응하는 어노테이션*/
+public interface Ex03_1_ArticleRepository extends JpaRepository<Article, Long> {
+
+}
